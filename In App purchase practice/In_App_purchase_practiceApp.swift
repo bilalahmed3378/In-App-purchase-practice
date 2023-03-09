04b960a011acc2dbf588a57f7d7ee3d6b055407e
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-03-09 15:00:43 +0500

    Initial Commit

---
+++ In App purchase practice/In_App_purchase_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_App_purchase_practiceApp.swift
+//  In App purchase practice
+//
+//  Created by Bilal Ahmed on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct In_App_purchase_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

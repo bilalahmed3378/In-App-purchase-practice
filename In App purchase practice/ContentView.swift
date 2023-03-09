da9d709077613433cc97c48b4bacc67b3f4d1b71
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-03-09 17:35:25 +0500

    huh

--- In App purchase practice/ContentView.swift
+++ In App purchase practice/ContentView.swift
@@ -17,43 +17,98 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             
-            Button(action: {
-                viewModel.purchase()
-            }, label: {
-                Text("Buy")
-
-            })
+            VStack{
+                
+                if let product = viewModel.products.first{
+                    Text(product.displayName)
+                    Text(product.description)
+                    Button(action: {
+                        if viewModel.purchasedIds.isEmpty{
+                            viewModel.purchase()
+                        }
+                    }, label: {
+                        Text(viewModel.purchasedIds.isEmpty ? "Buy Now \(product.displayPrice)" : "Purchsed")
+                        
+                    })
+                   
+                }
+               
+                
+            }
             .onAppear{
                 viewModel.fetchProducts()
-
+                
             }
-          
+            
         }
-    
-        
     }
    
 }
 
 
 class ViewModel : ObservableObject {
-    var products : [Product] = []
+   @Published var products : [Product] = []
+    @Published var purchasedIds : [String] = []
+
     func fetchProducts(){
         async{
             do{
-                let products = try await Product.products(for: ["com.temporary.id"])
-                self.products = products
+                let products = try await Product.products(for: ["com.temporary.id_neww"])
+                DispatchQueue.main.async {
+                    self.products = products
+                }
+                if let product = products.first{
+                    await isPurchased(product : product)
+                }
             }
             catch{
                 print(error)
             }
         }
     }
+    func isPurchased(product : Product) async {
+        
+       
+           guard let state = await product.currentEntitlement
+            else{
+               return
+           }
+        print("Checking state")
+            switch state {
+            case .verified(let transaction):
+                DispatchQueue.main.async {
+                    self.purchasedIds.append(transaction.productID)
+                }
+            case .unverified(_):
+                break
+            
+            }
+        
+    }
+    
     func purchase(){
         async{
             guard let product = products.first else {return}
         do{
             let result = try await product.purchase()
+            switch result {
+                
+            case .success(let verification) :
+                switch verification {
+                case .verified(let transaction) :
+                    DispatchQueue.main.async {
+                        self.purchasedIds.append(transaction.productID)
+                    }
+                case .unverified(_) :
+                    break
+                }
+            case .userCancelled :
+                break
+            case .pending :
+                break
+            @unknown default:
+                break
+            }
         }
         catch{
             print(error)
